238490c045bec95e2a710388c88b3fbf670cd1bd
hanshikag <hanshika@itemmaster.com>
2019-02-26 17:53:14 +0530

    JUnit Test cases improvisation

--- gojek/src/main/java/ClientApplication.java
+++ gojek/src/main/java/ClientApplication.java
@@ -3,7 +3,7 @@ import model.ProdDictionary;
 public class ClientApplication {
 
     public static void main(String[] args) throws Exception {
-        ProdDictionary prodDictionary = new ProdDictionary();
+        ProdDictionary prodDictionary = new ProdDictionary(10);
         prodDictionary.set(1,"Sony TV");
         prodDictionary.set(2,"Apple iphone");
 
